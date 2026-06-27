ce8ea58a8298e0d936b736958b388a2383ab0c21
Sakthi Priya B <129202823+sakthee07@users.noreply.github.com>
2026-06-27 11:14:56 +0530

    Create problem1.sql

---
+++ Day1/problem1.sql
@@ -0,0 +1,4 @@
+---Show the category_name and description from the categories table sorted by category_name. https://www.sql-practice.com/
+
+SELECT category_name,description  FROM categories 
+order by category_name;
