ce58080699620849a633b20d2c3d0f2d53ba882a
Sakthi Priya B <129202823+sakthee07@users.noreply.github.com>
2026-06-27 11:30:25 +0530

    Create problem5.sql

---
+++ Day1/problem5.sql
@@ -0,0 +1,9 @@
+--Show all the even numbered Order_id from the orders table
+
+select order_id 
+  from orders
+  where order_id%2 = 0;
+--or 
+SELECT order_id
+FROM orders
+WHERE mod(order_id,2)=0;
