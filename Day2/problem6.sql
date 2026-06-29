e1bfd8ccd2c3533c946a0be0bc4ce69b404e8e8f
Sakthi Priya B <129202823+sakthee07@users.noreply.github.com>
2026-06-29 22:05:41 +0530

    Create problem6.sql

---
+++ Day2/problem6.sql
@@ -0,0 +1,28 @@
+-- Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+
+SELECT
+  first_name,
+  last_name,
+  allergies
+FROM patients
+WHERE
+  allergies IN ('Penicillin', 'Morphine')
+ORDER BY
+  allergies,
+  first_name,
+  last_name;
+
+--or
+SELECT
+  first_name,
+  last_name,
+  allergies
+FROM
+  patients
+WHERE
+  allergies = 'Penicillin'
+  OR allergies = 'Morphine'
+ORDER BY
+  allergies ASC,
+  first_name ASC,
+  last_name ASC;
