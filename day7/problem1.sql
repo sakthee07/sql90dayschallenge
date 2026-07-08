00601f486c6d106b9fe73d468eb97cd963ceaaef
Sakthi Priya B <129202823+sakthee07@users.noreply.github.com>
2026-07-08 10:54:32 +0530

    Update problem1.sql

--- day7/problem1.sql
+++ day7/problem1.sql
@@ -11,3 +11,26 @@ on p.patient_id = a.patient_id
 join doctors d 
 on d.doctor_id = a.attending_doctor_id
 where a.diagnosis = 'Epilepsy' and d.first_name = 'Lisa' ;
+
+--or
+--using CTE
+
+with patient_table as (
+    SELECT
+      patients.patient_id,
+      patients.first_name,
+      patients.last_name,
+      admissions.attending_doctor_id
+    FROM patients
+      JOIN admissions ON patients.patient_id = admissions.patient_id
+    where
+      admissions.diagnosis = 'Epilepsy'
+  )
+select
+  patient_table.patient_id,
+  patient_table.first_name,
+  patient_table.last_name,
+  doctors.specialty
+from patient_table
+  JOIN doctors ON patient_table.attending_doctor_id = doctors.doctor_id
+WHERE doctors.first_name = 'Lisa';
